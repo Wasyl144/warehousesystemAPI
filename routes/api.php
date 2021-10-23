02dbc3180ac83a06910db49658e5199df98bccfd
Michal Frankiewicz <wasyl144@outlook.com>
2021-10-23 10:53:17 +0200

    added Laravle Permission package, and added simple test route to get a user

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user',function (Request $request) {
         return $request->user();
     });
+
+    Route::get('/user/{id}', [ProfileController::class, 'show']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
