56d6c38f493e5cfcb5740d76907d6acb4ff12441
Michal Frankiewicz <wasyl144@outlook.com>
2021-12-29 22:01:21 +0100

    fixed seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => 'admin',
         ]);
+        $this->call([
+           PermissionsSeeder::class,
+        ]);
     }
 }
